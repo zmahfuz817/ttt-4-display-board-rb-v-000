4f839e8c165fee7bfb82c5f72bb1c8db3dd6f1af
zmahfuz817 <zunayed.lm@gmail.com>
2018-03-09 14:57:28 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,8 @@ board = ["o", "X", " ", " ", " X ", " ", " X ", " o ", " "]
 puts "#{board[0]},#{board[1]},#{board[2]},#{board[3]},#{board[4]},#{board[5]},#{board[6]},#{board[7]},#{board[8]}"
 def display_board(board)
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
-  puts "--------------------------------------------"
+  puts "-----------"
   puts " #{board[3]}  | #{board[4]}  |  #{board[5]} "
-  puts "--------------------------------------------"
+  puts "-----------"
   puts " #{board[6]}  | #{board[7]}  | #{board[8]}  "
 end
